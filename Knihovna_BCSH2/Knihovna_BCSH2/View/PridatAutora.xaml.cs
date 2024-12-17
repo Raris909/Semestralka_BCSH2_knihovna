57b4756491cdfc3c396966ce21395a41c5f23a87
Raris909 <st69639@upce.cz>
2024-12-17 22:07:51 +0100

    AddAuthor now works in MVVM

--- Knihovna_BCSH2/Knihovna_BCSH2/View/PridatAutora.xaml.cs
+++ Knihovna_BCSH2/Knihovna_BCSH2/View/PridatAutora.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Knihovna_BCSH2.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,84 +20,18 @@ namespace Knihovna_BCSH2
     /// </summary>
     public partial class PridatAutora : Window
     {
-        private DatabaseHelper dbHelper = new DatabaseHelper();
-        private Autor currentAuthor;
         public PridatAutora(Autor author = null)
         {
             InitializeComponent();
-            if (author != null)
+
+            var viewModel = new PridatAutoraViewModel(author);
+            viewModel.CloseRequested += result =>
             {
-                currentAuthor = author;
-                JmenoTextBox.Text = currentAuthor.Jmeno;
-                PrijmeniTextBox.Text = currentAuthor.Prijmeni;
-                DatumNarozeniTextBox.Text = currentAuthor.DatumNarozeni.ToString("yyyy-MM-dd");
-                ZemeTextBox.Text = currentAuthor.Zeme;
-                Title = "Upravit autora";
-            }
-            else
-            {
-                Title = "Přidat autora";
-            }
-        }
-
-        private void OkButton_Click(object sender, RoutedEventArgs e)
-        {
-            string jmeno = JmenoTextBox.Text;
-            string prijmeni = PrijmeniTextBox.Text;
-            string datumNarozeniStr = DatumNarozeniTextBox.Text;
-            string zeme = ZemeTextBox.Text;
-
-            if (string.IsNullOrEmpty(jmeno) || string.IsNullOrEmpty(prijmeni) || string.IsNullOrEmpty(datumNarozeniStr) || string.IsNullOrEmpty(zeme))
-            {
-                MessageBox.Show("Vyplňte všechny povinné údaje.", "Chyba", MessageBoxButton.OK, MessageBoxImage.Warning);
-                return;
-            }
-
-            DateTime datumNarozeni;
-            if (!DateTime.TryParse(datumNarozeniStr, out datumNarozeni))
-            {
-                MessageBox.Show("Neplatný formát data narození.", "Chyba", MessageBoxButton.OK, MessageBoxImage.Warning);
-                return;
-            }
-
-            try
-            {
-                if (currentAuthor == null)
-                {
-                    var newAuthor = new Autor
-                    {
-                        Jmeno = jmeno,
-                        Prijmeni = prijmeni,
-                        DatumNarozeni = datumNarozeni,
-                        Zeme = zeme
-                    };
-
-                    dbHelper.AddAuthor(newAuthor);
-                    MessageBox.Show("Autor byl úspěšně přidán.", "Úspěch", MessageBoxButton.OK, MessageBoxImage.Information);
-                }
-                else
-                {
-                    currentAuthor.Jmeno = jmeno;
-                    currentAuthor.Prijmeni = prijmeni;
-                    currentAuthor.DatumNarozeni = datumNarozeni;
-                    currentAuthor.Zeme = zeme;
-
-                    dbHelper.UpdateAuthor(currentAuthor);
-                    MessageBox.Show("Autor byl úspěšně upraven.", "Úspěch", MessageBoxButton.OK, MessageBoxImage.Information);
-                }
-
-                DialogResult = true;
+                DialogResult = result;
                 Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Došlo k chybě při přidávání nebo úpravě autora: {ex.Message}", "Chyba", MessageBoxButton.OK, MessageBoxImage.Warning);
-            }
-        }
+            };
 
-        private void CancelButton_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
+            DataContext = viewModel;
         }
     }
 }
---
+++ Knihovna_BCSH2/Knihovna_BCSH2/ViewModel/PridatAutoraViewModel.cs
@@ -0,0 +1,148 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace Knihovna_BCSH2.ViewModel
+{
+    public class PridatAutoraViewModel : INotifyPropertyChanged
+    {
+        private readonly DatabaseHelper dbHelper = new DatabaseHelper();
+        private Autor _currentAuthor;
+
+        // Vlastnosti pro datovou vazbu
+        private string _jmeno;
+        public string Jmeno
+        {
+            get => _jmeno;
+            set { _jmeno = value; OnPropertyChanged(); }
+        }
+
+        private string _prijmeni;
+        public string Prijmeni
+        {
+            get => _prijmeni;
+            set { _prijmeni = value; OnPropertyChanged(); }
+        }
+
+        private string _datumNarozeni;
+        public string DatumNarozeni
+        {
+            get => _datumNarozeni;
+            set { _datumNarozeni = value; OnPropertyChanged(); }
+        }
+
+        private string _zeme;
+        public string Zeme
+        {
+            get => _zeme;
+            set { _zeme = value; OnPropertyChanged(); }
+        }
+
+        private string _title;
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+
+
+        // Příkazy
+        public ICommand OkCommand { get; }
+        public ICommand CancelCommand { get; }
+
+        public event Action<bool> CloseRequested;
+
+        // Konstruktor
+        public PridatAutoraViewModel(Autor author = null)
+        {
+            if (author != null)
+            {
+                _currentAuthor = author;
+                Jmeno = author.Jmeno;
+                Prijmeni = author.Prijmeni;
+                DatumNarozeni = author.DatumNarozeni.ToString("yyyy-MM-dd");
+                Zeme = author.Zeme;
+                Title = "Upravit autora";
+            }
+            else
+            {
+                _currentAuthor = null;
+                Title = "Přidat autora";
+            }
+
+            OkCommand = new RelayCommand<PridatAutora>(ExecuteOk);
+            CancelCommand = new RelayCommand<PridatAutora>(ExecuteCancel);
+        }
+
+        private void ExecuteOk(object parameter)
+        {
+            if (string.IsNullOrEmpty(Jmeno) || string.IsNullOrEmpty(Prijmeni) ||
+                string.IsNullOrEmpty(DatumNarozeni) || string.IsNullOrEmpty(Zeme))
+            {
+                MessageBox.Show("Vyplňte všechny povinné údaje.", "Chyba", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (!DateTime.TryParse(DatumNarozeni, out DateTime parsedDate))
+            {
+                MessageBox.Show("Neplatný formát data narození.", "Chyba", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            try
+            {
+                if (_currentAuthor == null)
+                {
+                    var newAuthor = new Autor
+                    {
+                        Jmeno = Jmeno,
+                        Prijmeni = Prijmeni,
+                        DatumNarozeni = parsedDate,
+                        Zeme = Zeme
+                    };
+
+                    dbHelper.AddAuthor(newAuthor);
+                    MessageBox.Show("Autor byl úspěšně přidán.", "Úspěch", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    _currentAuthor.Jmeno = Jmeno;
+                    _currentAuthor.Prijmeni = Prijmeni;
+                    _currentAuthor.DatumNarozeni = parsedDate;
+                    _currentAuthor.Zeme = Zeme;
+
+                    dbHelper.UpdateAuthor(_currentAuthor);
+                    MessageBox.Show("Autor byl úspěšně upraven.", "Úspěch", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+
+                CloseRequested?.Invoke(true);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Došlo k chybě při ukládání autora: {ex.Message}", "Chyba", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void ExecuteCancel(object parameter)
+        {
+            CloseRequested?.Invoke(false);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
